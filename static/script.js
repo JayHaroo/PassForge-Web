19358e169ca47aaa5aa5c5f3fb8c3de41a325456
M-Jey <mjeyict3@gmail.com>
2025-01-15 08:05:02 +0800

    add password generation functionality with a button in index.html

---
+++ static/script.js
@@ -0,0 +1,16 @@
+const saltchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const saltcharsnm = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const saltcharsnms = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+
+const passlen = 12;
+
+var password = "";
+
+const passwordField = document.querySelector("#password");
+
+function generate() {
+    while (password.length < passlen) {
+        password += saltchars[Math.floor(Math.random() * saltchars.length)];
+    }
+    passwordField.textContent = password;
+}
