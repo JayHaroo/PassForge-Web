d32f2b598513e8e2ecc2f1d47d0ee2fad83460d5
M-Jey <mjeyict3@gmail.com>
2025-01-19 07:44:28 +0800

    refactor: organize constants and variables for password generation

--- static/script.js
+++ static/script.js
@@ -1,20 +1,24 @@
+// Constant Values for password generation
 const saltchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const syms = "!@#$%^&*()_+";
 const nums = "0123456789";
 
+// Variables for password generation
 var passlen = 0;
-
 var password = "";
 
+
+// DOM Elements
 const passwordField = document.querySelector("#password");
 const symbolCheckbox = document.querySelector("#symbols");
 const numberCheckbox = document.querySelector("#numbers");
 const customCheckbox = document.querySelector("#custom");
 const customField = document.querySelector("#customField");
-
 const passlen8 = document.querySelector("#passlen12");
 const passlen16 = document.querySelector("#passlen16");
 
+/* this function is called when the generate button is clicked. 
+   It generates a password based on the user's input */
 function generate() {
     var customSaltChars = customField.value.replace(/\s/g, '');
     
